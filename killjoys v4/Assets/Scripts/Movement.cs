57db2c480ed07b6d5bef73aff44a6b467d0be901
Casey Cook <s3722194@student.rmit.edu.au>
2022-05-10 01:27:03 +1000

    working in 3d now

---
+++ killjoys v4/Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    // private InputController ControllerInput;
+    private PlayerInput ControllerInput;
+    [SerializeField]
+    private float speed;
+    private Rigidbody rb;
+
+    [SerializeField]
+    // so diagoonse arent faster
+    private float moveLimiter = 0.7f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ControllerInput = gameObject.GetOrAddComponent<InputController>();
+        ControllerInput = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 move = ControllerInput.actions["Movement"].ReadValue<Vector2>();
+
+
+        if (move.x != 0 && move.y != 0) // Check for diagonal movement
+        {
+            // limit movement speed diagonally, so you move at 70% speed
+            move.x *= moveLimiter;
+            move.y *= moveLimiter;
+        }
+
+        rb.velocity = new Vector3(move.x * speed, 0, move.y * speed);
+    }
+}
